2fd5d86e172a96857a1d5683d7eb29f04271cee9
Josha <angrypacket@gmail.com>
2015-11-13 14:59:38 -0800

    script uses $HOME instead

--- scr/deploy.sh
+++ scr/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-s3cmd --config=/Users/k/.s3bronsec  sync pub/. s3://www.bronsec.com --exclude '.DS_Store'
-s3cmd setacl --config=/Users/k/.s3bronsec s3://www.bronsec.com/ --acl-public --recursive
+
+s3cmd --config=$HOME/.s3bronsec  sync pub/. s3://www.bronsec.com --exclude '.DS_Store'
+s3cmd setacl --config=$HOME/.s3bronsec s3://www.bronsec.com/ --acl-public --recursive
