8b6b819fbd47ebd1fd5c2ca0475af32ac92c6446
kevin olson <acidjazz@gmail.com>
2015-11-13 16:58:46 -0800

    working menu

--- jst/index.js
+++ jst/index.js
@@ -2,6 +2,19 @@ var Index;
 
 Index = {
   i: function() {
-    return console.log('Index.i()');
+    console.log('Index.i()');
+    return Index.handlers();
+  },
+  handlers: function() {
+    return $('.logo, .menu > .item').on('click', Index.menu);
+  },
+  menu: function() {
+    var section;
+    section = $(".section." + ($(this).data('section')));
+    $('html, body').animate({
+      scrollTop: section.offset().top - 74
+    }, 200);
+    $('.menu > .item').removeClass('active');
+    return $(this).addClass('active');
   }
 };
